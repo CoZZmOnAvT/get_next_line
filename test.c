87db38cef8519c80749ff3cf9899f9315133bcf7
Pavlo GRITSENKO <pgritsen@e2r13p8.unit.ua>
2017-12-03 16:32:31 +0200

    Error handling added

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: pgritsen <pgritsen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 12:58:11 by pgritsen          #+#    #+#             */
-/*   Updated: 2017/11/25 13:14:28 by pgritsen         ###   ########.fr       */
+/*   Updated: 2017/12/03 16:31:54 by pgritsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,20 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-void	advanced(char *f1, char *f2, char *f3)
+int		advanced(char *f1, char *f2, char *f3)
 {
-	int		fd1, fd2, fd3;
+	int		fd1;
+	int		fd2;
+	int		fd3;
 	int		tmp;
 	char	*line;
 
 	if ((fd1 = open(f1, O_RDONLY)) == -1)
-		return ;
+		return (-1);
 	if ((fd2 = open(f2, O_RDONLY)) == -1)
-		return ;
+		return (-1);
 	if ((fd3 = open(f3, O_RDONLY)) == -1)
-		return ;
-
+		return (-1);
 	tmp = get_next_line(fd1, &line);
 	printf("FUNC RES - |%d|, STRING - |%s|\n", tmp, line);
 	tmp = get_next_line(fd2, &line);
@@ -37,6 +38,7 @@ void	advanced(char *f1, char *f2, char *f3)
 	printf("FUNC RES - |%d|, STRING - |%s|\n", tmp, line);
 	while (get_next_line(fd1, &line) > 0)
 		printf("FUNC RES - |%d|, STRING - |%s|\n", tmp, line);
+	return (0);
 }
 
 void	basic(char *filename)
@@ -51,12 +53,24 @@ void	basic(char *filename)
 		printf("FUNC RES - |%d|, STRING - |%s|\n", tmp, line);
 }
 
-void	custom()
+int		custom(void)
 {
 	char	*line;
 
 	while (get_next_line(0, &line) > 0)
 		printf("%s\n", line);
+	return (0);
+}
+
+void	error_handling(void)
+{
+	char	*line;
+
+	printf("ERROR HANDLING:\n");
+	printf("DESC - |%d|, FUNC RES - |%d|, STRING - |%s|\n", -4,
+		get_next_line(-4, &line), line);
+	printf("DESC - |%d|, FUNC RES - |%d|, STRING - |%s|\n", 14,
+		get_next_line(14, &line), line);
 }
 
 /*
@@ -68,10 +82,12 @@ void	custom()
 int		main(int ac, char **av)
 {
 	if (ac == 1)
-		custom();
-	else if (ac == 2)
+		return (custom());
+	if (ac == 2)
 		basic(av[1]);
 	else if (ac == 4)
-		advanced(av[1], av[2], av[3]);
+		return (advanced(av[1], av[2], av[3]));
+	printf("\n\n");
+	error_handling();
 	return (0);
 }
